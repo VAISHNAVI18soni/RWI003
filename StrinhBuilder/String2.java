3efc11179b8f0342ea3df1df33dba3c20134e31c
VAISHNAVI18soni <107538465+VAISHNAVI18soni@users.noreply.github.com>
2024-04-27 01:39:43 +0530

    Add files via upload

---
+++ StrinhBuilder/String2.java
@@ -0,0 +1,12 @@
+package StrinhBuilder;
+
+public class String2 {
+
+	public static void main(String[] args) {
+		StringBuilder sb=new StringBuilder();
+		System.out.println(sb.length());
+
+
+	}
+
+}
